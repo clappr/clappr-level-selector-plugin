6a1351628c656fcc0c19b86ae067ac430162645c
Lucas Mundim <lucas.mundim@corp.globo.com>
2015-03-05 13:55:45 -0300

    Update to use the new plugin template, removing underscore dependency

--- src/styler.js
+++ src/styler.js
@@ -1,11 +1,8 @@
-var $ = require('zepto');
-var _ = require('underscore');
 var JST = require('.././jst');
 
 var Styler = {
-  getStyleFor: function(name, options) {
-    options = options || {};
-    return $('<style></style>').html(_.template(JST.CSS[name])(options));
+  getStyleFor: function(name) {
+    return $('<style>').html(JST.CSS[name]);
   }
 };
 
