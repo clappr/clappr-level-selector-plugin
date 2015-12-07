a421083007131a818f3185d94102d225607e9600
Germano Fronza <germano@eventials.com>
2015-12-07 13:36:22 -0200

    fix render with currentLevel being 0

--- src/main.js
+++ src/main.js
@@ -64,7 +64,7 @@ export default class LevelSelector extends UICorePlugin {
   shouldRender() {
     if (!this.core.getCurrentPlayback()) return false
 
-    var respondsToCurrentLevel = !!this.core.getCurrentPlayback().currentLevel
+    var respondsToCurrentLevel = this.core.getCurrentPlayback().currentLevel !== undefined
     var hasLevels = !!(this.core.getCurrentPlayback().levels && this.core.getCurrentPlayback().levels.length > 0)
     return respondsToCurrentLevel && hasLevels
   }
